bea4b8f59be3ba5278b0420bb92428d03f1c3b8e
Michelle Robertson <michelle.lea.robertson@gmail.com>
2024-06-29 16:33:36 -0400

    Move links from header to footer and modify location and path for links.

---
+++ distractme-client/src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+function Footer() {
+  const location = useLocation();
+
+  const renderLinks = () => {
+    if (location.pathname === '/') {
+      return (
+        <ul>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      );
+    } else if (location.pathname === '/facts') {
+      return (
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      );
+    } else if (location.pathname === '/about') {
+      return (
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      );
+    }
+    return null; 
+  };
+
+  return (
+    <footer>
+      <nav>
+        {renderLinks()}
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
